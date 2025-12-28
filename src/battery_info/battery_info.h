50ea92a406e765826ebde17292bc61fbd30c3c50
MarcusPy827 <markus_verify@126.com>
2025-12-28 03:38:54 -0600

    [Resources] [Icons] Imported: Material symbols for battery

---
+++ src/battery_info/battery_info.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2025 MarcusPy827
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_BATTERY_INFO_BATTERY_INFO_H_
+#define SRC_BATTERY_INFO_BATTERY_INFO_H_
+
+#include <QString>
+
+namespace panel {
+namespace backend {
+
+class BatteryInfo {
+ public:
+  static int GetBatteryLevel();
+};
+
+}  // namespace backend
+}  // namespace panel
+
+#endif  // SRC_BATTERY_INFO_BATTERY_INFO_H_
